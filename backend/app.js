0b3a87273adb37ca01bba709c8e8aef21d73445e
replakcan <alper.makcan@gmail.com>
2025-07-11 18:26:53 +0300

    Imports new routers in appjs

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 const indexRouter = require('./routes/indexRouter')
+const postRouter = require('./routes/postRouter')
 const passport = require('passport')
+const isAuth = require('./auth/isAuth')
+const authorRouter = require('./routes/authorRouter')
+const usersRouter = require('./routes/usersRouter')
 require('./auth/jwtStrategy')
 
 app.use(express.json())
@@ -9,6 +13,9 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(passport.initialize())
 
+app.use('/users', isAuth, usersRouter)
+app.use('/author', isAuth, authorRouter)
+app.use('/posts', postRouter)
 app.use('/', indexRouter)
 
 app.use((err, req, res, next) => {
