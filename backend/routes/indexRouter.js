314a8c98d07659594d2d68bed7c3819f8c6a8f9e
replakcan <alper.makcan@gmail.com>
2025-07-11 18:22:43 +0300

    Defines new route for fetching all users

--- backend/routes/indexRouter.js
+++ backend/routes/indexRouter.js
@@ -3,12 +3,14 @@ const isAuth = require('../auth/isAuth')
 const indexRouter = Router()
 const indexController = require('../controllers/indexController')
 
-indexRouter.get('/', (req, res) => res.json({ msg: 'index-router home-page' }))
-
 indexRouter.post('/login', indexController.usersLoginPost)
 
 indexRouter.post('/register', indexController.usersRegisterPost)
 
 indexRouter.get('/secret', isAuth, indexController.usersSecretGet)
+
+indexRouter.get('/users', indexController.usersFindMany)
+
+indexRouter.get('/', (req, res) => res.json({ msg: 'index-router home-page' }))
 
 module.exports = indexRouter
