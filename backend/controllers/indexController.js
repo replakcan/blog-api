97fad5e2dc3d1a67ace7c1754f7d067a008f6824
replakcan <alper.makcan@gmail.com>
2025-07-11 14:48:26 +0300

    Creates router callbacks in controller and uses in the router

---
+++ backend/controllers/indexController.js
@@ -0,0 +1,54 @@
+const generateToken = require("../auth/generateToken")
+const prisma = require("../lib/prisma")
+const bcrypt = require('bcryptjs')
+
+exports.usersLoginPost = async (req, res, next) => {
+  const { username, password } = req.body
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        username,
+      },
+    })
+
+    if (!user) return res.status(401).json({ message: 'Invalid username' })
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if (!match) return res.status(401).json({ message: 'Invalid password' })
+
+    const token = generateToken(user)
+
+    res.json({ token })
+  } catch (error) {
+    return next(error)
+  }
+}
+
+exports.usersRegisterPost = async (req, res, next) => {
+  const { first_name, last_name, username, email, age, password } = req.body
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const newUser = await prisma.user.create({
+      data: {
+        first_name,
+        last_name,
+        username,
+        email,
+        age,
+        password: hashedPassword,
+      },
+    })
+
+    res.status(201).json(newUser)
+  } catch (error) {
+    return next(error)
+  }
+}
+
+exports.usersSecretGet = (req, res) => {
+  res.json({ message: `Hello ${req.user.username}, you made it to the secret dungeon!` })
+}
--- backend/routes/indexRouter.js
+++ backend/routes/indexRouter.js
@@ -1,6 +1,14 @@
 const { Router } = require('express')
+const isAuth = require('../auth/isAuth')
 const indexRouter = Router()
+const indexController = require('../controllers/indexController')
 
 indexRouter.get('/', (req, res) => res.json({ msg: 'index-router home-page' }))
+
+indexRouter.post('/login', indexController.usersLoginPost)
+
+indexRouter.post('/register', indexController.usersRegisterPost)
+
+indexRouter.get('/secret', isAuth, indexController.usersSecretGet)
 
 module.exports = indexRouter
