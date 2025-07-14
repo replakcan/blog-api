773fe6cfc70837afc646087c6da881f23931c368
replakcan <alper.makcan@gmail.com>
2025-07-14 17:27:17 +0300

    Updates root-page layout with display grid elements

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -1,12 +1,24 @@
+import '../styles/root.css'
 import { Outlet } from 'react-router-dom'
 import TestContext from '../test-context'
+import { useState } from 'react'
 
 function Root() {
+  const [currentUser, setCurrentUser] = useState()
+
   return (
     <TestContext.Provider value={{ user: currentUser }}>
-      <h1>Root component</h1>
-      <Outlet />
-    </>
+      <section className="root">
+        <header className="root-header">
+          <p>header</p>
+        </header>
+        <aside className="root-sidebar">
+          <p>sidebar</p>
+        </aside>
+        <main className="root-main">
+          <Outlet />
+        </main>
+      </section>
     </TestContext.Provider>
   )
 }
