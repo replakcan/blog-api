e6c02c74aa3e3408a8a10dce5023c98d77b1e3cf
replakcan <alper.makcan@gmail.com>
2025-07-14 17:28:18 +0300

    Creates loader function in home-page to load all posts

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -4,6 +4,8 @@ import ErrorPage from './routes/error-page'
 import HomePage from './routes/home-page'
 import LoginPage from './routes/login-page'
 import RegisterPage from './routes/register-page'
+import ContextTest from './routes/context-test'
+import { loader as feedLoader } from './routes/home-page'
 
 const routes = [
   {
@@ -18,6 +20,7 @@ const routes = [
           {
             path: 'home',
             element: <HomePage />,
+            loader: feedLoader,
           },
           {
             path: 'login',
--- frontend/src/routes/home-page.jsx
+++ frontend/src/routes/home-page.jsx
@@ -1,5 +1,35 @@
+import { Fragment } from 'react'
+import { useLoaderData } from 'react-router-dom'
+import { axiosInstance } from '../api/axiosInstance'
+
+// eslint-disable-next-line react-refresh/only-export-components
+export async function loader() {
+  try {
+    const response = await axiosInstance.get('posts')
+
+    const feed = response.data
+
+    return { feed }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 function HomePage() {
-  return <h1>Home Page</h1>
+  const { feed } = useLoaderData()
+
+  return (
+    <>
+      {feed.map((post) => {
+        return (
+          <Fragment key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.text}</p>
+          </Fragment>
+        )
+      })}
+    </>
+  )
 }
 
 export default HomePage
