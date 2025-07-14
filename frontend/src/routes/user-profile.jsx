31676da91cae8064648dbb46fa59b93b17124fd8
replakcan <alper.makcan@gmail.com>
2025-07-14 20:28:27 +0300

    Adds nav section in user-profile

--- frontend/src/routes/user-profile.jsx
+++ frontend/src/routes/user-profile.jsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react'
 import TestContext from '../test-context'
+import HeaderLink from '../components/header-link'
 
 export default function UserProfile() {
   const { user } = useContext(TestContext)
@@ -9,10 +10,15 @@ export default function UserProfile() {
   }
 
   const userFullname = `${user.first_name} ${user.last_name}`
-  
+
   return (
     <>
       <h1>USER DETAILS</h1>
+      <nav>
+        <HeaderLink to="/new-post" text="Create new post" />
+        <HeaderLink text="User posts" />
+        <HeaderLink text="User comments" />
+      </nav>
       <hr />
       <p>Fullname: {userFullname}</p>
       <p>Age: {user.age}</p>
