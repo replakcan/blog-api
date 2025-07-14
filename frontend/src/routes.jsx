ce8a674df1b64f51ff915c604f986a6b674ed6b8
replakcan <alper.makcan@gmail.com>
2025-07-15 00:18:05 +0300

    Adds user-profile child routes

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -7,6 +7,9 @@ import RegisterPage from './routes/register-page'
 import ContextTest from './routes/context-test'
 import { loader as feedLoader } from './routes/home-page'
 import UserProfile from './routes/user-profile'
+import NewPostForm from './routes/new-post-form'
+import UserPosts from './routes/user-posts'
+import UserComments from './routes/user-comments'
 
 const routes = [
   {
@@ -37,7 +40,22 @@ const routes = [
           },
           {
             path: 'profile',
-            element: <UserProfile />
+            element: <UserProfile />,
+            errorElement: <ErrorPage />,
+            children: [
+              {
+                path: 'new-post',
+                element: <NewPostForm />
+              },
+              {
+                path: 'posts',
+                element: <UserPosts />
+              },
+              {
+                path: 'comments',
+                element: <UserComments />
+              }
+            ]
           }
         ]
       }
