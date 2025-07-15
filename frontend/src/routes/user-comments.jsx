d5ac86e772129f605d626fc90530b21bea26fe7b
replakcan <alper.makcan@gmail.com>
2025-07-15 21:01:06 +0300

    Fetches posts and comments via backend-api

--- frontend/src/routes/user-comments.jsx
+++ frontend/src/routes/user-comments.jsx
@@ -1,11 +1,25 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import UserContext from '../user-context'
 import CommentCard from '../components/comment-card'
+import { axiosInstance } from '../api/axiosInstance'
 
 export default function UserComments() {
   const { user } = useContext(UserContext)
+  const [comments, setComments] = useState(user.comments)
 
-  const comments = user.comments
+  useEffect(() => {
+    const fetchUserComments = async () => {
+      try {
+        const res = await axiosInstance.get(`author/${user.id}/comments`)
+
+        setComments(res.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchUserComments()
+  }, [user.id])
 
   if (comments.length < 1)
     return (
--- frontend/src/routes/user-posts.jsx
+++ frontend/src/routes/user-posts.jsx
@@ -1,11 +1,25 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import UserContext from '../user-context'
 import PostCard from '../components/post-card'
+import { axiosInstance } from '../api/axiosInstance'
 
 export default function UserPosts() {
   const { user } = useContext(UserContext)
+  const [posts, setPosts] = useState(user.posts)
 
-  const posts = user.posts
+  useEffect(() => {
+    const fetchUserPosts = async () => {
+      try {
+        const res = await axiosInstance.get(`author/${user.id}/posts`)
+
+        setPosts(res.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchUserPosts()
+  }, [user.id])
 
   if (posts.length < 1)
     return (
