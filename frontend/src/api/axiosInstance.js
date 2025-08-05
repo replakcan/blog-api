a65387ad76b6183d63d7e9aeabbd604a722f6717
replakcan <alper.makcan@gmail.com>
2025-08-05 22:02:50 +0300

    Gets baseURL via env var

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const API_BASE_URL = import.meta.env.VITE_API_URL
+
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
